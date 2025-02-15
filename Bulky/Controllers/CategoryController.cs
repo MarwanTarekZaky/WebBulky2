b7a173f767578564023904f7e2d00f2f7b9fcd84
Marwan Tarek Zaky <marwantarekzaky@gmail.com>
2025-02-16 01:13:49 +0200

    lol

--- Bulky/Controllers/CategoryController.cs
+++ Bulky/Controllers/CategoryController.cs
@@ -22,12 +22,84 @@ public class CategoryController : Controller
     {
         return View();
     }
-
+    
     [HttpPost]
     public IActionResult Create(Category category)
     {
-        _context.Categories.Add(category);
-        _context.SaveChanges();
-        return RedirectToAction("Index");
+        if (category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("DisplayOrder", "Display Can not exactly equals Name ");
+        }
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            TempData["Success"] = "Category Created";
+            return RedirectToAction("Index", "Category");
+        }
+
+        return View(category);
     }
+    
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        Category? category = _context.Categories.Find(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(Category category)
+    {
+        
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            TempData["Success"] = "Category Updated";
+            return RedirectToAction("Index", "Category");
+        }
+
+        return View(category);
+    }
+    
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        Category? category = _context.Categories.Find(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
+    
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeletePost(int? id)
+    {
+        Category? category = _context.Categories.Find(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        else{
+            
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            TempData["Success"] = "Category Deleted";
+            return RedirectToAction("Index", "Category");
+        }
+    }
+    
+    
 }
--- Bulky/Models/Category.cs
+++ Bulky/Models/Category.cs
@@ -12,9 +12,11 @@ public class Category
 
     [Display(Name = "Name")]
     [Required]
+    [MaxLength(30)]
     public string Name { get; set; }
     
     
     [Display(Name = "Display Order")]
+    [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100")]
     public int DisplayOrder { get; set; }
 }
